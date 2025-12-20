fe42160b8e9eae7f82fa3a90872fe0dcf6b5eaf1
“d19920527” <d362514@gmail.com>
2025-12-20 16:28:21 +0800

    第一個專案

---
+++ src/main/java/com/example/eggcakeshopapi/EggcakeShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eggcakeshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggcakeShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggcakeShopApiApplication.class, args);
+	}
+
+}
